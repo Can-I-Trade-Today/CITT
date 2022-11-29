e9d8f0a08b8830b5584b83e5482be390cfbb103c
Hessel Amann <hessel.amann@gmail.com>
2022-11-29 19:57:23 +0100

    init

---
+++ apps/cron/express/cron-job.js
@@ -0,0 +1,12 @@
+"use strict";
+const { schedule } = require("@netlify/functions");
+
+const handler = async function (event, context) {
+  console.log("Received event:", event);
+
+  return {
+    statusCode: 200,
+  };
+};
+
+module.exports.handler = schedule("@hourly", handler);
---
+++ apps/cron/server-local.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const app = require("./express/cron-job");
+
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
