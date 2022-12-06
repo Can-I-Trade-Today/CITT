3b46b037e74b4e742c2752be4d9fc3d4ccc81b23
Earde Bearda <eardelb@hotmail.com>
2022-12-06 21:37:55 +0100

    .

--- apps/api/express/server.js
+++ apps/api/express/server.js
@@ -4,11 +4,20 @@ const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+dotenv.config({ path: '../../.env' });
+
+//MongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DATABASE_UN}:${process.env.DATABASE_PW}@citt-cluster.xllgjxx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const router = express.Router();
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello from Earde and Hessel!</h1>");
+  const result = await client.db("StockData").collection("IWO").find({}).limit(50).toArray();
+  res.write(`<h1>${JSON.stringify(result)}</h1>`);
   res.end();
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
